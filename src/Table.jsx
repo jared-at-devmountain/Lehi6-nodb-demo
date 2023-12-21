c2d1919dbe0d886504d352568ed70cfea3b490d8
Jared Collier <collierjared@gmail.com>
2023-12-21 14:19:13 -0700

    makes Row comp generation dynamic

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,16 +1,48 @@
+import { useState } from 'react'
 import Header from './Header.jsx'
 import Row from './Row.jsx'
 import AddButton from './AddButton.jsx'
 
+let data = [
+    {
+        description: 'Firebreather',
+        rate: 100,
+        hours: 60,
+        id: 1
+    },
+    {
+        description: 'Shoe Shiner',
+        rate: 120,
+        hours: 1,
+        id: 2
+    },
+    {
+        description: 'Tent Master',
+        rate: 30,
+        hours: 100,
+        id: 3
+    }
+]
+
 export default function Table() {
+
+    const [tableData, setTableData] = useState(data)
+
     return (
         <>
             <table>
                 <Header/>
                 <tbody>
-                    <Row description={'Firebreather'} rate={100} hours={60}/>
-                    <Row description={'Shoe Shiner'} rate={120} hours={1}/>
-                    <Row description={'Tent Master'} rate={30} hours={100}/>
+                    { tableData.map((jobObj) => {
+                        return <Row
+                            key={jobObj.id}
+                            description={jobObj.description}
+                            rate={jobObj.rate}
+                            hours={jobObj.hours}
+                        />
+                    })
+
+                    }
                 </tbody>
             </table>
             <AddButton/>
