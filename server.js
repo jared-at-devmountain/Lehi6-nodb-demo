3da1845f42d63fc0982b15166d418f08116ff80a
Jared Collier <collierjared@gmail.com>
2023-12-22 01:22:12 -0700

    has new back end with a put route (unused by front end)

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import ViteExpress from 'vite-express'
+
+const app = express()
+
+app.use(express.json())
+
+const db = [
+    {
+        description: 'Firebreather',
+        rate: 100,
+        hours: 60,
+        id: 1
+    },
+    {
+        description: 'Shoe Shiner',
+        rate: 120,
+        hours: 1,
+        id: 2
+    },
+    {
+        description: 'Tent Master',
+        rate: 30,
+        hours: 100,
+        id: 3
+    }
+]
+
+app.put('/edit-job/:id', (req, res) => {
+    let id = +req.params.id
+    let editedJob = req.body
+
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === id) {
+            db.splice(i, 1, editedJob)
+            break
+        }
+    }
+
+    res.status(200).send(db)
+})
+
+// app.listen(3000, () => {console.log('listening on 3000')})
+ViteExpress.listen(app, 3000, () => {console.log('listening on 3000')})
